249b51b7be7a63a050409d20a37f9bced4944f9c
zxfjd3g <258147149@qq.com>
2018-09-28 14:48:05 +0800

    router

--- src-mint-ui/main.js
+++ src-mint-ui/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import {Button} from 'mint-ui'
 
 import App from './App.vue'
-import router from './router'
 
 // 注册全局组件
 Vue.component(Button.name, Button)
@@ -14,6 +13,5 @@ new Vue({
   components: {
     App
   },
-  template: '<App/>', // 将<App/>渲染到页面的el元素中
-  router // 注册路由器: <router-link> / <router-view>
+  template: '<App/>' // 将<App/>渲染到页面的el元素中
 })
